8216f953a2a742db249f70387f1ac001016b887d
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2022-11-12 17:56:33 +0900

    middleware basic study

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-console.log("Hello NodeJS");
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+// application settings ...
+
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`)
+    next();
+}
+
+const privateMiddleware = (req, res, next) => {
+    if (req.url === "/protected") return res.send("<h1>Not Allowed</h1>");
+
+    console.log("Allowed, You may continue.");
+    next();
+}
+
+const handleHome = (req, res) => {
+    return res.send("<h1>I still Love You 💜.</h1>");
+}
+
+const handleProtected = (req, res) => {
+    return res.send("Welcome to the private lounge.");
+}
+
+app.use(logger);
+app.use(privateMiddleware);
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
+
+const handleListening = () => {
+    console.log(`🎸 Server Listening on Port http://localhost:${PORT}`);
+}
+
+app.listen(PORT, handleListening);
