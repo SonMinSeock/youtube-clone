b410ec0d8e5c1852de18e6c0e64a4b5803306943
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2022-11-17 17:34:02 +0900

    Router Design, Router Make

---
+++ README.md
@@ -0,0 +1,16 @@
+# Youtube Clone
+# Route Design! 💜
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/edit -> Edit User
+/users/remove -> Delete User
+
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/videos/comments -> Comment on a Video
+/videos/comments/delete -> Delete a Comment of a Video
--- src/index.js
+++ src/index.js
@@ -1,30 +1,25 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
+
 const PORT = 4000;
 
 // make express application.
 const app = express();
-
 // application settings ...
-
 const logger = morgan("dev");
-
-const handleHome = (req, res) => {
-    return res.send("<h1>I still Love You 💜.</h1>");
-}
-
-const handleLogin = (req, res) => {
-    return res.send("<h1>Login Page~ 🎸</h1>");
-}
-
 app.use(logger);
-// app.use(privateMiddleware);
 
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+// Routers
+
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 const handleListening = () => {
     console.log(`🎸 Server Listening on Port http://localhost:${PORT}`);
 }
 
-app.listen(PORT, handleListening);
+app.listen(PORT, handleListening); 
