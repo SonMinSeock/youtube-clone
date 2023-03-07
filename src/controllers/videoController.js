fe2c42687970eba7cf0770f509a5d140d30a9e07
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-03-07 18:19:22 +0900

    16.4 API Router part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -185,3 +185,9 @@ export const registerView = async (req, res) => {
   await video.save();
   return res.sendStatus(200);
 };
+
+export const createComment = (req, res) => {
+  console.log(req.params);
+  console.log(req.body);
+  res.end();
+};
